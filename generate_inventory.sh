cb8760fcbe4c917a33ecc118dcc6f712210b5567
Mazaich <mazaich@yahoo.com>
2025-12-21 06:32:54 +0300

    Delete generate_inventory.sh

--- generate_inventory.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-echo "Ручная настройка inventory"
-echo "Скопируйте IP адреса из вывода команды:"
-echo "yc compute instance list"
-echo "Затем отредактируйте файл ansible/inventory/hosts вручную"
